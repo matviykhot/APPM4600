cc5731da09aa2371aa7225dcb647534113013a6d
matviykhot <matviy.khotyaintsev.7157@student.uu.se>
2024-09-19 10:49:17 -0600

    Added subrourine for convergence check fixedpt_example.py

--- Labs/Lab 4/fixedpt_example.py
+++ Labs/Lab 4/fixedpt_example.py
@@ -11,44 +11,65 @@ def driver():
 #fixed point is alpha2 = 3.09... 
 
      Nmax = 100
-     tol = 1e-6
+     tol = 1e-8
 
 # test f1 '''
      x0 = 0.0
-     [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+     [xstar,ier, guesses] = fixedpt(f1,x0,tol,Nmax)
+     print('')
      print('the approximate fixed point is:',xstar)
      print('f1(xstar):',f1(xstar))
      print('Error message reads:',ier)
+     
+     if ier == 0:
+        [fit, diff1, diff2] = convergence(xstar, guesses)
     
 #test f2 '''
      x0 = 0.0
-     [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+     [xstar,ier, guesses] = fixedpt(f2,x0,tol,Nmax)
      print('the approximate fixed point is:',xstar)
      print('f2(xstar):',f2(xstar))
      print('Error message reads:',ier)
-
-
+     if ier == 0:
+        [fit, diff1, diff2] = convergence(xstar, guesses)
 
 # define routines
 def fixedpt(f,x0,tol,Nmax):
-
-    ''' x0 = initial guess''' 
+    guesses = np.zeros((Nmax,1))
+    ''' x0 = initial guess'''
     ''' Nmax = max number of iterations'''
     ''' tol = stopping tolerance'''
 
     count = 0
     while (count <Nmax):
-       count = count +1
        x1 = f(x0)
+       count = count +1
        if (abs(x1-x0) <tol):
           xstar = x1
           ier = 0
-          return [xstar,ier]
+          return [xstar,ier, guesses]
+       guesses[count-1] = x1
        x0 = x1
-
+    
     xstar = x1
     ier = 1
-    return [xstar, ier]
+    return [xstar, ier, guesses]
+    
+def convergence(xstar, guesses):
+    diff1 = np.abs(guesses[1::]-xstar)
+    diff2 = np.abs(guesses[0:-1]-xstar)
+   # print("diff1: ")
+    #print(diff1)
+   # print("diff2: ")
+    #print(diff2)
+    
+    fit = np.polyfit(np.log(diff2.flatten()),np.log(diff1.flatten()),1)
+    print('')
+    print("Order")
+    print("(log|p_{n+1}-p|) = log(lambda) + alpha*log|p_{n}-p| where")
+    print("lambda = " + str(np.exp(fit[1])))
+    print("alpha = " + str(fit[0]))
+    return [fit, diff1, diff2]
     
 
 driver()
